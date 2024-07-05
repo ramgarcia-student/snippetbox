3a92bbd50e53f58d909a68c60b90cdb34bb33850
ramgarcia-student <Ramon.Garcia@alu.frt.utn.edu.ar>
2024-07-04 23:15:55 -0300

    handler and server implementation

---
+++ go.mod
@@ -0,0 +1,3 @@
+module snippetbox.ramgarcia-student.net
+
+go 1.22.4
---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/{$}", home)
+	mux.HandleFunc("/snippet/view/{id}", snippetView) // Add the {id} wildcard segment
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	log.Println("starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	msg := fmt.Sprintf("Display a specific snippet with ID %d...", id)
+	w.Write([]byte(msg))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a new snippet..."))
+}
